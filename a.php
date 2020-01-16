616571788b1c19ec4add0857c5cdf40e842eb008
mhieupham1 <mhieupham1@gmail.com>
2020-01-16 17:52:40 +0700

    fix code number to word

--- a.php
+++ a.php
@@ -1,8 +1,8 @@
 <?php
 function convertToNumber($number){
-    $unitNumber1 = ['','ngàn','triệu','tỉ'];
-    $singleNumber = ['','một','hai','ba','bốn','năm','sáu','bảy','tám','chín','mười','mười một','mười hai','mười ba','mười bốn','mười năm','mười sáu','mười bảy','mười tám','mười chín'];
-    $unitNumber = ['2'=>'chục','3'=>'trăm'];
+    $unitNumber1 = ['','nghìn','triệu','tỉ'];
+    $singleNumber = ['','một','hai','ba','bốn','năm','sáu','bảy','tám','chín','mười','mười một','mười hai','mười ba','mười bốn','mười lăm','mười sáu','mười bảy','mười tám','mười chín'];
+    $numberThanTwenty = ['','mốt','hai','ba','tư','lăm','sáu','bảy','tám','chín','mười','mười một','mười hai','mười ba','mười bốn','mười lăm','mười sáu','mười bảy','mười tám','mười chín'];
     $strNumber = (string) ($number);
     $lengthNumber = strlen(strval($strNumber));
     $level =(int) (($lengthNumber+2)/3);
@@ -14,32 +14,23 @@ function convertToNumber($number){
         $level--;
         $hundred = (int) ($numArr[$i] / 100);
         $strHundred = $hundred ? $singleNumber[$hundred].' trăm': '';
-        $ten = (int) ($numArr[$i] % 100);
-        if($ten < 20){
-            if($ten > 10){
-                $word .= $strHundred.' '.$singleNumber[$ten].' '.$unitNumber1[$level].' ';
-            }else if($ten == 0){
-                $word .= $strHundred.''.$singleNumber[$ten].' '.$unitNumber1[$level].' ';
-            }else{
-                $word .= $strHundred.' lẻ '.$singleNumber[$ten].' '.$unitNumber1[$level].' ';
-            }
+        $double = (int) ($numArr[$i] % 100);
+        $doubleInt = (int) ($double/10);
+        if($double == 0){
+            $doubleNumber = $singleNumber[$double%10];
+        }elseif ($double < 10){
+            $doubleNumber = 'lẻ '.$singleNumber[$double%10];
+        }elseif (($double >= 10) && ($double <20)){
+            $doubleNumber = $singleNumber[$double];
         }else{
-            $double = (int) ($ten / 10);
-            $strDouble = $double ? $singleNumber[$double].' mươi': '';
-            if($double < 2){
-                $single = (int) ($ten % 10);
-                $strSingle = $single ?' lẻ '.$singleNumber[$single] : '';
-            }else{
-                $single = (int) ($ten % 10);
-                $strSingle = $single ?' '.$singleNumber[$single] : '';
-            }
-            $word .= $strHundred.' '.$strDouble.''.$strSingle.' '.$unitNumber1[$level].' ';
+            $doubleNumber = $singleNumber[$doubleInt].' mươi '.$numberThanTwenty[$double%10];
         }
+        $numArr[$i] == 000 ? : $word .= $strHundred.' '.$doubleNumber.' '.$unitNumber1[$level].' ';
     }
     $word = preg_replace("/^\s+/", "", $word);
     echo ucfirst($word);
 }
-convertToNumber(600525702815);
+convertToNumber(800123000812);
 
 
 
